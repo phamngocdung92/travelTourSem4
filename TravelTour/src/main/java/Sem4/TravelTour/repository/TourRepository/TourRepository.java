ee8604d0c1044c3ae43e1b7abbc0ff48a4de3c69
callmesheep <dungntth2206013@fpt.edu.vn>
2024-10-25 18:22:57 +0700

    tour update

--- TravelTour/src/main/java/Sem4/TravelTour/repository/TourRepository/TourRepository.java
+++ TravelTour/src/main/java/Sem4/TravelTour/repository/TourRepository/TourRepository.java
@@ -1,4 +1,21 @@
 package Sem4.TravelTour.repository.TourRepository;
 
-public interface TourRepository {
+import Sem4.TravelTour.entity.Tour;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TourRepository extends JpaRepository<Tour, Long>
+{
+    List<Tour> findByStatusTrue();
+    @Query(value = "Select t.* From tour t \r\n"
+            + "left join rates r on t.tour_id = r.tour_id\r\n"
+            + "group by t.tour_id , t.name\r\n"
+            + "Order by  avg(r.rating) desc, RAND()", nativeQuery = true)
+    List<Tour> findTourRated();
+    Tour save(Tour tour);
+    List<Tour> findByStatusTrueOrderBySoldDesc();
+    Optional<Tour> findById(Long id);
 }
--- TravelTour/src/main/java/Sem4/TravelTour/service/TourService/TourService.java
+++ TravelTour/src/main/java/Sem4/TravelTour/service/TourService/TourService.java
@@ -1,4 +1,16 @@
 package Sem4.TravelTour.service.TourService;
 
+import Sem4.TravelTour.entity.Tour;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TourService {
+
+    boolean existsById(Long id);
+    List<Tour> getAll();
+    Tour save(Tour tour);
+    List<Tour> getBestSeller();
+    List<Tour> getRate();
+    Optional<Tour> findById(Long id);
 }
