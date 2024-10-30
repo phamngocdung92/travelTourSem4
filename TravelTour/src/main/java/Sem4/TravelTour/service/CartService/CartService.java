734c6ca885f61c986f07ece90bf7a8387ab919c2
callmesheep <dungntth2206013@fpt.edu.vn>
2024-10-30 19:21:49 +0700

    cart

--- TravelTour/src/main/java/Sem4/TravelTour/service/CartService/CartService.java
+++ TravelTour/src/main/java/Sem4/TravelTour/service/CartService/CartService.java
@@ -1,7 +1,14 @@
 package Sem4.TravelTour.service.CartService;
 
 import Sem4.TravelTour.entity.Cart;
+import Sem4.TravelTour.entity.Tour;
+import Sem4.TravelTour.entity.User;
+
+import java.util.Optional;
 
 public interface CartService {
     Cart save(Cart cart);
+    Cart findByUser(User user);
+    boolean existsById(Long id);
+    Optional<Cart> findById(Long id);
 }
--- TravelTour/src/main/java/Sem4/TravelTour/service/CartService/CartServiceImpl.java
+++ TravelTour/src/main/java/Sem4/TravelTour/service/CartService/CartServiceImpl.java
@@ -1,9 +1,13 @@
 package Sem4.TravelTour.service.CartService;
 
 import Sem4.TravelTour.entity.Cart;
+import Sem4.TravelTour.entity.Tour;
+import Sem4.TravelTour.entity.User;
 import Sem4.TravelTour.repository.CartRepository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CartServiceImpl implements CartService {
@@ -16,4 +20,15 @@ public class CartServiceImpl implements CartService {
     public Cart save(Cart cart) {
         return cartRepository.save(cart);
     }
+    @Override
+    public Cart findByUser(User user) {return  cartRepository.findByUser(user);}
+    @Override
+    public boolean existsById(Long id) {
+        return cartRepository.existsById(id);
+    }
+    @Override
+    public Optional<Cart> findById(Long id) {
+        return cartRepository.findById(id);
+    }
 }
+
